2821e00cada209cc352b47538b5d1cf2ee0b1145
fikatuwu <fikat.ft@gmail.com>
2026-09-14 08:21:52 +0700

    Add worker.js entrypoint and update wrangler to latest for Workers Assets

---
+++ worker.js
@@ -0,0 +1,16 @@
+﻿export default {
+  async fetch(request, env) {
+    try {
+      const response = await env.ASSETS.fetch(request);
+      if (response.status === 404) {
+        const url = new URL(request.url);
+        if (!url.pathname.includes('.')) {
+          return await env.ASSETS.fetch(new Request(new URL('/index.html', request.url), request));
+        }
+      }
+      return response;
+    } catch (err) {
+      return new Response("Asset fetch error: " + err.message, { status: 500 });
+    }
+  }
+};
